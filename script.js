a669966417d70a4a9e31a94c1875cc98239ca75a
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-03-18 18:50:09 -0400

    secondpush
    tried to add buttons and variable but now nothing is working

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWlkYW5hcm1zdHJvbmciLCJhIjoiY2xzam1tczhoMnJqMzJpbzZ4OWh0bmI4dyJ9.2pEogvW_3XlwcsMh4kMfCQ'; //Add default public map token from your Mapbox account
+
 const map = new mapboxgl.Map({
 container: 'my-map', //this ID points to my map container so that CSS can display this across the whol website
 style: 'mapbox://styles/mapbox/streets-v12', //this is a street style but I could use any style
 center: [-79.375, 43.712], // starting position [lng, lat]
 zoom: 9, // starting zoom
 });
+
+// Adding Variables with input data
+let collision;
+// Fetch GeoJSON from GitHub and strore response
+// Fetch GeoJSON from URL and store response
+fetch('https://raw.githubusercontent.com/Adnarmstrng/Lab4/ggr472-lab4-main/main/data/pedcyc_collision_06-21.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response); //Check response in console
+        collision = response; // Store geojson as variable using URL from fetch response
+    });
