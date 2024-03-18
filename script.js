3e31857bfab6d5f4182c6c001f00c672bca8edbe
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-03-18 19:25:28 -0400

    removed buttons

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWlkYW5hcm1zdHJvbmciLCJhIjoiY2xzam1tczhoMnJqMzJpbzZ4OWh0bmI4dyJ9.2pEogvW_3XlwcsMh4kMfCQ'; //Add default public map token from your Mapbox account
-
 const map = new mapboxgl.Map({
-container: 'my-map', //this ID points to my map container so that CSS can display this across the whole website
+container: 'my-map', //this ID points to my map container so that CSS can display this across the whol website
 style: 'mapbox://styles/mapbox/streets-v12', //this is a street style but I could use any style
 center: [-79.375, 43.712], // starting position [lng, lat]
 zoom: 9, // starting zoom
 });
-
 // Adding Variables with input data
 let collision;
 // Fetch GeoJSON from GitHub and strore response
