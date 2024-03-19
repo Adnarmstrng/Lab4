e2a5d04b9927b6d70b02f6bf4ddbe3ed3230b8de
Aidan Armstrong <139584588+Adnarmstrng@users.noreply.github.com>
2024-03-19 12:45:29 -0400

    Final prototype website

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Lab4
- 
+
+In lab 4, we learned about building on the fly anlysis of inputed GIS data for a website. The API we used to do the analysis was the Turf library. The Turf library interfaces with Node and browser webdata so that you can have real time spatial analyis. In our lab, we do not have a dataset on collisions in Toronto which could be used to locate and address dangerous road intersections. In this case, the dataset is simple because it is not changing. Potentially, we could do this same analysis using a GIS software on this dataset. However, if we wanted the dataset to constantly be updating and published to the web, then displaying the data as a wbeseite using Turf is advantageous. In this lab we always create undefined variables so that the code can adapt to changing dataset content and the final analysis will not break.
+
+
+
+The source of the data comes from "https://github.com/smith-lg/ggr472-lab4/blob/main/data/pedcyc_collision_06-21.geojson" which is a repository containing "point locations of road collisions involving pedestrian and cyclists between 2006 and 2021 in Toronto"
+
+
--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWlkYW5hcm1zdHJvbmciLCJhIjoiY2xzam1tczhoMnJqMzJpbzZ4OWh0bmI4dyJ9.2pEogvW_3XlwcsMh4kMfCQ'; //Add default public map token from your Mapbox account
 const map = new mapboxgl.Map({
-    container: 'my-map', //this ID points to my map container so that CSS can display this across the whol website
+    container: 'my-map', //this ID points to my map container so that CSS can display this across the whole website
     style: 'mapbox://styles/mapbox/streets-v12', //this is a street style but I could use any style
     center: [-79.375, 43.712], // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 // Adding Variables with input data
-let collision;
+let collision; //we create an empty variable called collision so that we don't hardcode a variable. In this way maybe we want the data to be constantly updating, so that when we use the fetch method, it stores the most recent data as the collision variable
 // Fetch GeoJSON from GitHub and strore response
-// Fetch GeoJSON from URL and store response
 fetch('https://raw.githubusercontent.com/Adnarmstrng/Lab4/main/ggr472-lab4-main/data/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
-        console.log(response); //Check response in console
+        console.log(response); //Check response in console: this can be used for debugging. 
         collision = response; // Store geojson as variable using URL from fetch response
     });
-
+// Start doing GIS manipluation of collision variable and displaying analysis onto the website body
 map.on('load', () => {
 
-    let bboxgeojson;
-    let bbox = turf.envelope(collision);
-    let bboxscaled = turf.transformScale(bbox, 1.10);
+    let bboxgeojson; //we want to create an ampty variable so that we ensure that whatever data we are using for the collision dataset gets an updated envelope size each time we run the website
+    let bbox = turf.envelope(collision); //calculate the envelope size based on the dispersion of point data
+    let bboxscaled = turf.transformScale(bbox, 1.10); //assign the envelope extent to the empty variable and scale it 
     bboxgeojson = {
         "type": "FeatureCollection",
         "features": [bboxscaled]
@@ -28,16 +27,16 @@ map.on('load', () => {
 
     //Create HexGrid//
 
-    let bboxcoords = [bboxscaled.geometry.coordinates[0][0][0],
+    let bboxcoords = [bboxscaled.geometry.coordinates[0][0][0], //the hexgrid needs to be bounded so we use the envelope to set the bounding. We call the coordinates method of geometry and then index through to the corners and pull their coordinates. Then we assign them to the hexgrid boundaries
     bboxscaled.geometry.coordinates[0][0][1],
     bboxscaled.geometry.coordinates[0][2][0],
     bboxscaled.geometry.coordinates[0][2][1]];
-    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: 'kilometers' });
+    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, { units: 'kilometers' }); //create the hexgrid
 
     // Collect properties from points//
-    let collishex = turf.collect(hexgeojson, collision, '_id', 'values');
+    let collishex = turf.collect(hexgeojson, collision, '_id', 'values'); //we want the hexgrid to store the number of collisions within a hex
 
-    // count
+    // count the number of collisions 
     let maxcollis = 0;
 
     collishex.features.forEach((feature) => {
@@ -48,6 +47,8 @@ map.on('load', () => {
         }
     })
 
+
+//Displaying the data using the map on load event listener
     map.addSource('collis-hex', {
         type: "geojson",
         data: hexgeojson
@@ -69,7 +70,7 @@ map.on('load', () => {
             'fill-outline-color': 'white'
         }
     })
-
+//adding interactions to the map by allowing the user to click on a map and a pop-up menue will occur with the data calculated under the 'count collisions' section
     map.on('click', 'collis-hex-fill', (e) => {
         new mapboxgl.Popup()
             .setLngLat(e.lnglat)
